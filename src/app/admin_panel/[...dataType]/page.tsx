e6d7d6867f3b32123256c46b6aaafc8d65b8177c
BuildTools <unconfigured@null.spigotmc.org>
2024-04-17 16:03:17 +0200

    Testing

--- src/app/admin_panel/[...dataType]/page.tsx
+++ src/app/admin_panel/[...dataType]/page.tsx
@@ -7,6 +7,8 @@ import ManufacturerOfAmmunitionEditor from "@/components/AdminPanel/Manufacturer
 import ManufacturerOfWeaponEditor from "@/components/AdminPanel/ManufacturerOfWeaponEditor/ManufacturerOfWeaponEditor";
 import WeaponEditor from "@/components/AdminPanel/WeaponEditor/WeaponEditor";
 
+export const dynamic = "force-dynamic";
+
 interface componentProps {
   params: { dataType: string[] };
 }
