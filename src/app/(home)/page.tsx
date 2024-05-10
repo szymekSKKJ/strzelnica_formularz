0eeefbb20d30faf68ab0df4e459004e39af10d46
BuildTools <unconfigured@null.spigotmc.org>
2024-05-10 11:50:15 +0200

    Adding current stage

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,14 @@
-import AdminPanel from "@/components/AdminPanel/AdminPanel";
+import styles from "./styles.module.scss";
 import ReservationVisitForm from "@/components/ReservationVisitForm/ReservationVisitForm";
 
 const HomePage = () => {
   return (
-    <>
+    <div className={`${styles.homePage}`}>
+      <h1>
+        Strzelnica Precision <i className="fa-sharp fa-regular fa-bullseye-arrow"></i>
+      </h1>
       <ReservationVisitForm></ReservationVisitForm>
-    </>
+    </div>
   );
 };
 
